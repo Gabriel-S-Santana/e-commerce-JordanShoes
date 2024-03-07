36cfb44431bd243447da46872ce705a71d607141
Gabriel-S-Santana <gabrielsouzasantana2@gmail.com>
2024-03-06 21:02:41 -0300

    add: first-commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,14 @@
+import { MainComponent } from './../main/main.component';
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeaderComponent, MainComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
